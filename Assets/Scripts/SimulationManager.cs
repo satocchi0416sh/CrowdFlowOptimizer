e60788edfff0eb31a1334ef803f54ab84670037a
satocchi0416sh <satoyoshi44@gmail.com>
2024-10-30 18:35:31 +0900

    feat: エージェントを複数のスポーンポイントから出せるように

--- Assets/Scripts/SimulationManager.cs
+++ Assets/Scripts/SimulationManager.cs
@@ -4,22 +4,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[Serializable]
+public class SpawnPoint
+{
+    public Transform Transform;
+    public int NumberOfAgents;
+}
+
 public class SimulationManager : MonoBehaviour
 {
+    
     private float _timeToExit;
     private List<Agent> _agents = new();
 
     [SerializeField] private GameObject _agentPrefab;
     [SerializeField] private Transform _target;
-    [SerializeField] private Transform _spawnPoint;
-    [SerializeField] private int _numberOfAgents;
     [SerializeField] private Text _timeText;
+    [SerializeField] private SpawnPoint[] _spawnPoints;
 
     private void Start()
     {
-        for (int i = 0; i < _numberOfAgents; i++)
+        foreach (var spawnPoint in _spawnPoints)
         {
-            GameObject agentGameObject = Instantiate(_agentPrefab, _spawnPoint.position, Quaternion.identity);
+            SpawnAgents(spawnPoint.Transform, spawnPoint.NumberOfAgents);
+        }
+    }
+
+    private void SpawnAgents(Transform spawnPoint, int numberOfAgents)
+    {
+        for (int i = 0; i < numberOfAgents; i++)
+        {
+            GameObject agentGameObject = Instantiate(_agentPrefab, spawnPoint.position, Quaternion.identity);
             Agent agent = agentGameObject.GetComponent<Agent>();
             agent.InitializeAgent(_target);
             _agents.Add(agent);
