90ef83e9955c92bb8c4af088309c491e3d08e416
satocchi0416sh <satoyoshi44@gmail.com>
2024-12-06 17:08:43 +0900

    feat: 前方のエージェントから一定距離を空けて歩くように

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -6,13 +6,18 @@ using UnityEngine.AI;
 
 public class Agent : MonoBehaviour
 {
-    [SerializeField]
-    private NavMeshAgent _navMeshAgent;
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+    [SerializeField] private float _beamLength = 1f;
+    [SerializeField] private LayerMask _targetLayer;
     private Transform _target;
+    private float _defaultSpeed;
+
+    private bool _isStopped = false;
 
     public void InitializeAgent(Transform target)
     {
         _target = target;
+        _defaultSpeed = _navMeshAgent.speed;
     }
 
     private void Update()
@@ -20,6 +25,45 @@ public class Agent : MonoBehaviour
         if (_target != null)
         {
             _navMeshAgent.SetDestination(_target.position);
+        }
+
+        ShootBeam();
+    }
+
+    private void ShootBeam()
+    {
+        Ray ray = new Ray(transform.position + Vector3.up, transform.forward);
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position + Vector3.up, transform.forward * _beamLength, Color.red);
+
+        if (Physics.Raycast(ray, out hit, _beamLength, _targetLayer))
+        {
+            if (hit.collider.CompareTag("Agent"))
+            {
+                StopAgent();
+                return;
+            }
+        }
+
+        ResumeAgent();
+    }
+
+    private void StopAgent()
+    {
+        if (!_isStopped)
+        {
+            _navMeshAgent.speed = 0f;
+            _isStopped = true;
+        }
+    }
+
+    private void ResumeAgent()
+    {
+        if (_isStopped)
+        {
+            _navMeshAgent.speed = _defaultSpeed;
+            _isStopped = false;
         }
     }
 
